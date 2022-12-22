8991a2cb753886e6b1f545ebf08ac45b791837f1
Juan Murua Olalde <juan@susosise.es>
2022-12-22 17:38:54 +0100

    Añadir consulta de APIs
    nota: Se ha commiteado un archivo de ejemplo para 'configuracion.json'. Para usarlo, cada cual tendrá que copiarse el archivo,  renombrar la copia (quitandole lo de '-ejemplo-sin-secretos') y ponerle dentro su propia API key de openweathermap.org

--- MiniAppWeb/js/otroCode.js
+++ MiniAppWeb/js/otroCode.js
@@ -11,6 +11,12 @@ window.addEventListener("load", () => {
         ProcesarPizza();
     });
 
+    // const formularioDeGeolocalizacionYMetereologia = document.querySelector("#formularioDeGeolocalizacionYMetereologia");
+    // formularioDeGeolocalizacionYMetereologia.addEventListener("submit", (event) => {
+    //     event.preventDefault;
+    //     ObtenerCoordenadasYMetereologia();
+    // });
+
     function CalcularParalelepipedo() {
         const datos = new FormData(formularioDeParalelepipedos);
         const unLado = Number(datos.get("unLado"));
@@ -67,3 +73,114 @@ window.addEventListener("load", () => {
         alert(respuesta.join(""));
     }
 });
+
+let CONFIGURACION;
+fetch("./configuracion.json")
+    .then((response) => {
+        if (!response.ok) {
+            alert("No se ha podido leer la configuracion:\n" + response.status + " " + response.statusText);
+        }
+        return response.json();
+    })
+    .then((jsonData) => {
+        CONFIGURACION = jsonData;
+    })
+    .catch((error) => alert("Se ha producido un error al procesar la configuracion:\n" + error.message));
+
+function ObtenerCoordenadasYMetereologia() {
+    const poblacion = document.getElementById("poblacion").value;
+    const codigoPais = document.getElementById("codigoPais").value;
+
+    const urlParaGeocodificacion =
+        "http://api.openweathermap.org/geo/1.0/direct" +
+        "?q=" +
+        poblacion +
+        "," +
+        codigoPais +
+        "&appid=" +
+        CONFIGURACION["weatherAPIkey"];
+    fetch(urlParaGeocodificacion)
+        .then((response) => {
+            if (!response.ok) {
+                alert(
+                    "No se ha podido obtener datos de la API de geolocalización:\n" +
+                        response.status +
+                        " " +
+                        response.statusText
+                );
+            }
+            return response.json();
+        })
+        .then((jsonData) => {
+            if (jsonData.length === 1) {
+                const timestamp = new Date(Date.now());
+                console.log(timestamp.toISOString(), "Se ha geolocalizado ", JSON.stringify(jsonData));
+                const latitud = jsonData[0]["lat"];
+                const longitud = jsonData[0]["lon"];
+                ObtenerMetereologia(latitud, longitud);
+            } else {
+                document.getElementById("contenedorRespuestaAPI").innerHTML =
+                    "<p>Los datos metereológicos solo se pueden consultar en una localización.</p>" +
+                    "<p>Y no se ha obtenido una respuesta clara a la geolocalización de [" +
+                    poblacion +
+                    " , " +
+                    codigoPais +
+                    "]:</p>" +
+                    "<p>" +
+                    JSON.stringify(jsonData) +
+                    "</p>";
+            }
+        })
+        .catch((error) => alert("Se ha producido un error en la API de geolocalización:\n" + error.message));
+}
+
+function ObtenerMetereologia(latitud, longitud) {
+    const urlParaMetereologia =
+        "https://api.openweathermap.org/data/2.5/weather?lat=" +
+        latitud +
+        "&lon=" +
+        longitud +
+        "&units=metric" +
+        "&lang=es" +
+        "&appid=" +
+        CONFIGURACION["weatherAPIkey"];
+    fetch(urlParaMetereologia)
+        .then((response) => {
+            if (!response.ok) {
+                alert("No se ha podido obtener datos de la API metereologica:\n" + response.status + " " + response.statusText);
+            }
+            return response.json();
+        })
+        .then((jsonData) => {
+            const temperaturaActual = jsonData["main"]["temp"];
+            const humedadActual = jsonData["main"]["humidity"];
+            const vientoVelocidad = jsonData["wind"]["speed"];
+            const vientoDireccion = jsonData["wind"]["deg"];
+            //Me salian valores raros en las horas de salida y puesta del Sol, asi es que no las uso.
+            const salidaDelSol = new Date(jsonData["sys"]["sunrise"]);
+            const puestaDelSol = new Date(jsonData["sys"]["sunset"]);
+            const timestamp = new Date(Date.now());
+            document.getElementById("contenedorRespuestaAPI").innerHTML =
+                "<p>Geolocalización: latitud " +
+                latitud +
+                " , longitud " +
+                longitud +
+                "</p>" +
+                "<p> Temperatura: " +
+                temperaturaActual +
+                "°C </p>" +
+                "<p> Humedad: " +
+                humedadActual +
+                "% </p>" +
+                "<p> Viento: " +
+                vientoVelocidad +
+                "m/s , soplando desde " +
+                vientoDireccion +
+                "°" +
+                "</p>" +
+                "<p><small>Consulta realizada el " +
+                timestamp.toLocaleString() +
+                "</small></p>";
+        })
+        .catch((error) => alert("Se ha producido un error en la API metereologica:\n" + error.message));
+}
